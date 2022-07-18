7330ef0ffe5543c1285b8a3ee2e74be1dffdf30d
leeejungmin <ljm3453@gmail.com>
2022-07-18 19:47:19 +0900

    ok

--- src/main/java/com/example/demo/testController.java
+++ src/main/java/com/example/demo/testController.java
@@ -5,8 +5,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController()
 public class testController {
-    @GetMapping("/jung")
+    @GetMapping("/")
     public String test(){
+        return "this is deploy test by test";
+    }
+    @GetMapping("/jung")
+    public String jung(){
         return "this is deploy test by jung";
     }
 }
